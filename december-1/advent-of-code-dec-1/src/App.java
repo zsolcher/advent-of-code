f5f24194117716a14b4e65c44b58ecff77d6c613
Zach Solcher <zsolcher@gmail.com>
2021-12-02 00:24:39 -0600

    finished part 2, cleaned up with functions

--- december-1/advent-of-code-dec-1/src/App.java
+++ december-1/advent-of-code-dec-1/src/App.java
@@ -1,32 +1,63 @@
-
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        File input = new File("bin/input.txt");
+        File input = new File("december-1/advent-of-code-dec-1/src/input.txt");
         System.out.println("Total number of measurements: " + input.length());
-        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
-            int numIncreasesInMeasurement = 0;
-            Integer previousMeasurement = null;
-            String currMeasurement;
-            while ((currMeasurement = br.readLine()) != null) {
-                if(previousMeasurement != null) {
-                    Integer currNum = Integer.parseInt(currMeasurement);
-                    System.out.println("(prevNum,currNum):(" + previousMeasurement + "," + currNum + ")");
-                    if(currNum > previousMeasurement) {
-                        numIncreasesInMeasurement++;
-                    }
-                    previousMeasurement = currNum;
-                } else {
-                    previousMeasurement = Integer.parseInt(currMeasurement);
+        System.out.println("Total number of increases in measurements: " + getNumIncreasesInMeasurements(input));
+        System.out.println("Total number of increases in measurements (by 3): " + getNumIncreasesInMeasurementsByThree(input));
+    }
+
+
+    public static int getNumIncreasesInMeasurements(File input) {
+        return getNumberOfIncreasesFromList(getNumberListFromFile(input));
+    }
+
+    public static int getNumIncreasesInMeasurementsByThree(File input) {
+        ArrayList<Integer> measurements = getNumberListFromFile(input);
+        ArrayList<Integer> measurementsBy3 = getTriplesSumsFromList(measurements);
+        return getNumberOfIncreasesFromList(measurementsBy3);
+    }
+
+    public static ArrayList<Integer> getTriplesSumsFromList(ArrayList<Integer> list) {
+        ArrayList<Integer> sums = new ArrayList<>();
+        for(int i = 0; i < list.size()-2; i++) {
+            Integer sum = list.get(i) + list.get(i+1) + list.get(i+2);
+            sums.add(sum);
+        }
+        return sums;
+    }
+
+    public static int getNumberOfIncreasesFromList(ArrayList<Integer> list) {
+        int numIncreases = 0;
+        if(list.size() > 1) {
+            for(int i = 1; i < list.size(); i++) {
+                if(list.get(i-1) < list.get(i)) {
+                    numIncreases++;
                 }
             }
-            System.out.println("Total number of increases in measurements: " + numIncreasesInMeasurement);
+        }
+        return numIncreases;
+    }
+
+    public static ArrayList<Integer> getNumberListFromFile(File file) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String currMeasurement;
+            while ((currMeasurement = br.readLine()) != null) {
+                res.add(Integer.parseInt(currMeasurement));
+            }
         }  catch (Exception e) {
             System.out.println("Exception occured: \n" + e.getMessage());
         }
+
+        return res;
     }
+
 }
+
+//Total number of increases in measurements: 1477
