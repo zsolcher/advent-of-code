85957d53d14aae8fad8b45e30e30ed841ebd4f81
Zach Solcher <zsolcher@gmail.com>
2021-12-02 15:06:53 -0600

    cleaned up dec 2 solution with OO design

---
+++ december-2/src/App.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        File input = new File("december-2/src/input.txt");
+        LinkedList<String> submarineCourse = getQueueFromFile(input);
+        System.out.println("Total number of commands: " + submarineCourse.size());
+
+        System.out.println("Starting submarine journey!");
+        Submarine submarine = new Submarine();
+        submarine.executeCoursePlan(submarineCourse);
+
+        System.out.println("Submarine final position...");
+        System.out.println("Submarine position:" + submarine.getHorizontalPosition());
+        System.out.println("Submarine depth:" + submarine.getDepth());
+        System.out.println("AoC answer is:" + submarine.getDepth() * submarine.getHorizontalPosition());
+    }
+
+    public static LinkedList<String> getQueueFromFile(File file) {
+        LinkedList<String> res = new LinkedList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String currCommand;
+            while ((currCommand = br.readLine()) != null) {
+                res.addLast(currCommand);
+            }
+        }  catch (Exception e) {
+            System.out.println("Exception occured: \n" + e.getMessage());
+        }
+        return res;
+    }
+
+}
+
+//Total number of increases in measurements: 1477
---
+++ december-2/src/Submarine.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+
+public class Submarine {
+    
+    private int depth = 0;
+    private int horizontalPosition = 0;
+    private int aim = 0;
+
+    public Submarine() {
+    }
+
+    public void executeCoursePlan(LinkedList<String> coursePlan) {
+        while(coursePlan.size() > 0) {
+            String[] currCommand = coursePlan.pollFirst().split(" ");
+            String commandType = currCommand[0];
+            Integer commandValue = Integer.parseInt(currCommand[1]);
+            switch(commandType) {
+                case "forward":
+                    horizontalPosition += commandValue;
+                    break;
+                case "down":
+                    depth += commandValue;
+                    break;
+                case "up":
+                    depth -= commandValue;
+                    break;
+                default:
+                    break;
+            }
+            // System.out.println("Submarine reached next checkpoint...");
+            // System.out.println("Submarine position:" + horizontalPosition);
+            // System.out.println("Submarine depth:" + depth);
+        }
+    }
+
+    public void executeCoursePlanWithAim(LinkedList<String> coursePlan) {
+        // TODO
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int getHorizontalPosition() {
+        return horizontalPosition;
+    }
+}
